6d6d5fb07e645b0af59b8dba1d1574834996ebcb
Ting Sun <sunting.05@gmail.com>
2018-09-26 16:45:15 +0100

    updated environment settings.

--- README.rst
+++ README.rst
@@ -4,6 +4,9 @@ Boundary-Layer Meteorology Course @ University of Reading
 ----------------------------------------------------------
 
 
+
+
+
 .. toctree::
   lectures/2_Evaporation_PenmanMonteith
   lectures/DataAnalysis_BLM
--- conf.py
+++ conf.py
@@ -76,7 +76,36 @@ language = None
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
 
-exclude_patterns = []
+# Exclude build directory and Jupyter backup files:
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
+# Don't add .txt suffix to source files (available for Sphinx >= 1.5):
+html_sourcelink_suffix = ''
+
+# Execute notebooks before conversion: 'always', 'never', 'auto' (default)
+#nbsphinx_execute = 'never'
+
+# Use this kernel instead of the one stored in the notebook metadata:
+#nbsphinx_kernel_name = 'python3'
+
+# List of arguments to be passed to the kernel that executes the notebooks:
+#nbsphinx_execute_arguments = ['--InlineBackend.figure_formats={"png", "pdf"}']
+
+# If True, the build process is continued even if an exception occurs:
+#nbsphinx_allow_errors = True
+
+# Controls when a cell will time out (defaults to 30; use -1 for no timeout):
+#nbsphinx_timeout = 60
+
+# Default Pygments lexer for syntax highlighting in code cells:
+#nbsphinx_codecell_lexer = 'ipython3'
+
+# Width of input/output prompts used in CSS:
+#nbsphinx_prompt_width = '8ex'
+
+# If window is narrower than this, input/output prompts are on separate lines:
+#nbsphinx_responsive_width = '700px'
+
 # tags.add('html')
 # if tags.has('html'):
 #     exclude_patterns = ['references.rst']
@@ -117,6 +146,37 @@ rst_prolog = """
 #
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = ["_themes"]
+
+
+# This is processed by Jinja2 and inserted before each notebook
+nbsphinx_prolog = r"""
+{% set docname = env.doc2path(env.docname, base='doc') %}
+.. only:: html
+    .. role:: raw-html(raw)
+        :format: html
+    .. nbinfo::
+        This page was generated from `{{ docname }}`__.
+        Interactive online version:
+        :raw-html:`<a href="https://mybinder.org/v2/gh/Urban-Meteorology-Reading/BLM/{{ env.config.release }}?filepath={{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge.svg" style="vertical-align:text-bottom"></a>`
+    __ https://github.com/Urban-Meteorology-Reading/BLM/blob/
+        {{ env.config.release }}/{{ docname }}
+.. raw:: latex
+    \vfil\penalty-1\vfilneg
+    \vspace{\baselineskip}
+    \textcolor{gray}{The following section was generated from
+    \texttt{\strut{}{{ docname }}}\\[-0.5\baselineskip]
+    \noindent\rule{\textwidth}{0.4pt}}
+    \vspace{-2\baselineskip}
+"""
+
+# This is processed by Jinja2 and inserted after each notebook
+nbsphinx_epilog = r"""
+.. raw:: latex
+    \textcolor{gray}{\noindent\rule{\textwidth}{0.4pt}\\
+    \hbox{}\hfill End of
+    \texttt{\strut{}{{ env.doc2path(env.docname, base='doc') }}}}
+    \vfil\penalty-1\vfilneg
+"""
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
